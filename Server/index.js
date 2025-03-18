7d60052bb74999db7d422a421f904bacfacf6624
jayakrishna0511 <dontharaboinajayakrishna@gmail.com>
2025-03-18 13:23:09 +0530

    fixed the cors issue in server

--- Server/index.js
+++ Server/index.js
@@ -11,10 +11,22 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST','PUT','DELETE'],
-    credentials:true
-}));
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:5173',
+        'https://emp-management-weld.vercel.app'
+      ];
+      
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
+  
 
 app.use(express.json());
 app.use(cookieParser())
