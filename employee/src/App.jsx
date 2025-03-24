af1d2ff8c9da9d0018bcf0c2c35c1b731bf10cf6
jayakrishna0511 <dontharaboinajayakrishna@gmail.com>
2025-03-24 22:07:49 +0530

    added protected route not to go directly to login page after entering dashboard

--- employee/src/App.jsx
+++ employee/src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './Components/Login'
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+import Login from './Components/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './Components/Dashboard';
 import Home from './Components/Home';
 import Employee from './Components/Employee';
@@ -18,42 +18,55 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { CookiesProvider } from 'react-cookie';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-function App() {
 
+function App() {
   return (
     <>
-     <CookiesProvider defaultSetOptions={{ path: '/' }}>
-      <BrowserRouter>
-        <Routes>
-        <Route path='/' element={<Start />} />                
-          <Route path='/adminlogin' element={ <Login />} />
-          <Route path='/employee_login' element={<EmployeeLogin />} />
+      <CookiesProvider defaultSetOptions={{ path: '/' }}>
+        <BrowserRouter>
+          <Routes>
+            {/* Public route - Landing page */}
+            <Route path='/' element={<Start />} />                
+
+            {/* Login route - Redirect if logged in */}
+            <Route path='/adminlogin' element={
+              <PrivateRoute isPublic={true}>
+                <Login />
+              </PrivateRoute>
+            } />
+
+            {/* Employee login route - Redirect if logged in */}
+            <Route path='/employee_login' element={
+              <PrivateRoute isPublic={true}>
+                <EmployeeLogin />
+              </PrivateRoute>
+            } />
+
+            {/* Employee detail route */}
             <Route path='/employee_detail/:id' element={<EmployeeDetail />} />
-          <Route path='/dashboard' element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
+
+            {/* Protected dashboard routes */}
+            <Route path='/dashboard' element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
             }>
-            <Route path='' element={<Home />} />
-            <Route path='/dashboard/employee' element={<Employee />} />
-            <Route path='/dashboard/category' element={<Category />} />
-            <Route path='/dashboard/profile' element={<Profile />} />
-            <Route path='/dashboard/add-category' element={<AddCategory />} />
-            <Route path='/dashboard/add_employee' element={<AddEmployee />} />
-            <Route path='/dashboard/edit-employee/:id' element={<EditEmployee />} />
-          </Route>
+              <Route index element={<Home />} />
+              <Route path='employee' element={<Employee />} />
+              <Route path='category' element={<Category />} />
+              <Route path='profile' element={<Profile />} />
+              <Route path='add-category' element={<AddCategory />} />
+              <Route path='add_employee' element={<AddEmployee />} />
+              <Route path='edit-employee/:id' element={<EditEmployee />} />
+            </Route>
+          </Routes>
 
-          {/* for toast */}
-        
-        <Route path="/employee/detail/:id" element={<EmployeeDetail />} />
-      </Routes>
-      <ToastContainer position="top-right" autoClose={5000} />
-
-        
-      </BrowserRouter>
+          {/* Toast notification container */}
+          <ToastContainer position="top-right" autoClose={5000} />
+        </BrowserRouter>
       </CookiesProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
